bb59baea86bce54f05bdbfcfd310607b590f426e
Ji Chen <jaykchen@gmail.com>
2023-05-23 21:57:58 -0400

    token counted, prompt updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-use dotenv::dotenv;
 use github_flows::{
     get_octo, listen_to_event,
     octocrab::models::events::payload::{IssueCommentEventAction, IssuesEventAction},
-    EventPayload, GithubLogin,
+    EventPayload,
+    GithubLogin::Default,
 };
 use openai_flows::{
     chat::{ChatModel, ChatOptions},
     OpenAIFlows,
 };
 use slack_flows::send_message_to_channel;
+use dotenv::dotenv;
 use std::env;
 use tiktoken_rs::cl100k_base;
 
@@ -17,14 +18,14 @@ use tiktoken_rs::cl100k_base;
 pub async fn run() {
     dotenv().ok();
 
-    let github_login = env::var("github_login").unwrap_or("alabulei1".to_string());
+    let _github_login = env::var("github_login").unwrap_or("alabulei1".to_string());
     let github_owner = env::var("github_owner").unwrap_or("alabulei1".to_string());
     let github_repo = env::var("github_repo").unwrap_or("a-test".to_string());
     let raw_trigger_word = env::var("trigger_word").unwrap_or("flows summarize".to_string());
-    let trigger_word = format!("@{github_login} {raw_trigger_word}");
+    let trigger_word = format!("@{_github_login} {raw_trigger_word}");
 
     listen_to_event(
-        &GithubLogin::Default,
+        &Default,
         &github_owner,
         &github_repo,
         vec!["issues", "issue_comment"],
@@ -37,27 +38,23 @@ async fn handler(trigger: &str, owner: &str, repo: &str, payload: EventPayload) 
     let slack_workspace = env::var("slack_workspace").unwrap_or("secondstate".to_string());
     let slack_channel = env::var("slack_channel").unwrap_or("github-status".to_string());
 
-    // let github_owner = env::var("github_owner").unwrap_or("alabulei1".to_string());
-    // let github_repo = env::var("github_repo").unwrap_or("a-test".to_string());
-
     // let n_days = env::var("number").unwrap().parse::<u64>().unwrap_or(2445);
 
-    let mut issue_number = 0u64;
-
+    let mut issue = None;
     match payload {
         EventPayload::IssuesEvent(e) => {
             if e.action != IssuesEventAction::Closed
-                && e.issue.body.unwrap_or("".to_string()).contains(&trigger)
+                && e.issue.clone().body.unwrap_or("".to_string()).contains(&trigger)
             {
-                issue_number = e.issue.number;
+                issue = Some(e.issue);
             }
         }
 
         EventPayload::IssueCommentEvent(e) => {
             if e.action != IssueCommentEventAction::Deleted
-                && e.comment.body.unwrap_or("".to_string()).contains(&trigger)
+                && e.comment.clone().body.unwrap_or("".to_string()).contains(&trigger)
             {
-                issue_number = e.issue.number;
+                issue = Some(e.issue);
             }
         }
         _ => {}
@@ -65,17 +62,17 @@ async fn handler(trigger: &str, owner: &str, repo: &str, payload: EventPayload) 
 
     // send_message_to_channel("ik8", "ch_in", issue_number.to_string());
 
-    if issue_number > 0 {
+    if let Some(issue) = issue {
         let mut openai = OpenAIFlows::new();
         openai.set_retry_times(3);
 
-        let octocrab = get_octo(&GithubLogin::Default);
+        let octocrab = get_octo(&Default);
         let issues_handle = octocrab.issues(owner, repo);
-        let issue = issues_handle.get(issue_number).await.unwrap();
 
         let issue_creator_name = issue.user.login;
         let mut issue_creator_role = "".to_string();
         issue_creator_role = issue.author_association;
+        let issue_number = issue.number;
         let issue_title = issue.title;
         let issue_body = issue.body.unwrap_or("".to_string());
         let issue_url = issue.html_url;
@@ -88,24 +85,21 @@ async fn handler(trigger: &str, owner: &str, repo: &str, payload: EventPayload) 
 
         let bpe = cl100k_base().unwrap();
 
-        let mut feed_tokens_map = String::new();
-        // let mut feed_tokens_map = Vec::new();
+        let mut feed_tokens_map = Vec::new();
 
-        let issue_creator_input = format!("issue creator {issue_creator_name} has role {issue_creator_role}, filed the issue titled {issue_title}, with labels {labels}, posting: {issue_body}");
+        let issue_creator_input = format!("User '{issue_creator_name}', who holds the role of '{issue_creator_role}', has submitted an issue titled '{issue_title}', labeled as '{labels}', with the following post: '{issue_body}'.");
 
         let mut tokens = bpe.encode_ordinary(&issue_creator_input);
-        // feed_tokens_map.append(&mut tokens);
-        feed_tokens_map.push_str(&issue_creator_input);
+        feed_tokens_map.append(&mut tokens);
 
         match issues_handle.list_comments(issue_number).send().await {
             Ok(pages) => {
                 for comment in pages.items {
-                    let comment_body = comment.body.unwrap();
+                    let comment_body = comment.body.unwrap_or("".to_string());
                     let commenter = comment.user.login;
                     let commenter_input = format!("{commenter} commented: {comment_body}");
                     let mut tokens = bpe.encode_ordinary(&commenter_input);
-                    // feed_tokens_map.append(&mut tokens);
-                    feed_tokens_map.push_str(&commenter_input);
+                    feed_tokens_map.append(&mut tokens);
                 }
             }
 
@@ -113,7 +107,7 @@ async fn handler(trigger: &str, owner: &str, repo: &str, payload: EventPayload) 
         }
 
         let chat_id = format!("Issue#{issue_number}");
-        let system = &format!("You are the co-owner of a github repo, you monitor new issues by analyzing the title, body text, labels and its context");
+        let system = &format!("As an AI co-owner of a GitHub repository, you are responsible for conducting a comprehensive analysis of GitHub issues. Your analytic focus encompasses distinct elements, including the issue's title, associated labels, body text, the identity of the issue's creator, their role, and the nature of the comments on the issue. Utilizing these data points, your task is to generate a succinct, context-aware summary of the issue.");
 
         let co = ChatOptions {
             model: ChatModel::GPT35Turbo,
@@ -121,24 +115,23 @@ async fn handler(trigger: &str, owner: &str, repo: &str, payload: EventPayload) 
             system_prompt: Some(system),
         };
 
-        // let check_text = bpe.decode(feed_tokens_map.clone()).unwrap();
-        // send_message_to_channel("ik8", "ch_in", check_text.clone());
+        let check_text = bpe.decode(feed_tokens_map.clone()).unwrap();
+        send_message_to_channel("ik8", "ch_in", check_text.clone());
 
-        let total_tokens_count = feed_tokens_map.split_whitespace().count();
+        let total_tokens_count = feed_tokens_map.len();
         let mut _summary = "".to_string();
 
-        if total_tokens_count > 1000 {
-            let mut token_vec = feed_tokens_map.split_whitespace().collect::<Vec<_>>();
+        if total_tokens_count > 2000 {
+            let mut token_vec = feed_tokens_map;
             let mut map_out = "".to_string();
 
             while !token_vec.is_empty() {
-                let drain_to = std::cmp::min(token_vec.len(), 1000);
+                let drain_to = std::cmp::min(token_vec.len(), 2000);
                 let token_chunk = token_vec.drain(0..drain_to).collect::<Vec<_>>();
 
-                let text_chunk = token_chunk.join(" ");
-                // let text_chunk = bpe.decode(token_chunk).unwrap();
+                let text_chunk = bpe.decode(token_chunk).unwrap();
 
-                let map_question = format!("The issue is titled {issue_title}, with one chunk of the body text or comment text {text_chunk}. Please focus on the main points of the comment, any proposed solutions, and any consensus or disagreements among the commenters. Please summarize key information in this section.");
+                let map_question = format!("Given the issue titled '{issue_title}' and a particular segment of body or comment text '{text_chunk}', focus on extracting the central arguments, proposed solutions, and instances of agreement or conflict among the participants. Generate an interim summary capturing the essential information in this section. This will be used later to form a comprehensive summary of the entire discussion.");
 
                 match openai.chat_completion(&chat_id, &map_question, &co).await {
                     Ok(r) => {
@@ -149,7 +142,7 @@ async fn handler(trigger: &str, owner: &str, repo: &str, payload: EventPayload) 
                 }
             }
 
-            let reduce_question = format!("{issue_creator_name} with role {issue_creator_role} filed the issue titled {issue_title}, labeled {labels}, here are the key info you extracted from issue body text and comments in chunks {map_out}, please focus on the main points of the comments, any proposed solutions, and any consensus or disagreements among the commenters. Please make a concise summary for this issue to facilitate the next action.");
+            let reduce_question = format!("User '{issue_creator_name}', in the role of '{issue_creator_role}', has filed an issue titled '{issue_title}', labeled as '{labels}'. The key information you've extracted from the issue's body text and comments in segmented form are: {map_out}. Concentrate on the principal arguments, suggested solutions, and areas of consensus or disagreement among the participants. From these elements, generate a concise summary of the entire issue to inform the next course of action.");
 
             match openai
                 .chat_completion(&chat_id, &reduce_question, &co)
@@ -162,10 +155,9 @@ async fn handler(trigger: &str, owner: &str, repo: &str, payload: EventPayload) 
             }
             send_message_to_channel("ik8", "ch_out", _summary.clone());
         } else {
-            let issue_body = feed_tokens_map;
-            // let issue_body = bpe.decode(feed_tokens_map).unwrap();
+            let issue_body = bpe.decode(feed_tokens_map).unwrap();
 
-            let question = format!("{issue_body}, please focus on the main points of the comments, any proposed solutions, and any consensus or disagreements among the commenters. Please make a concise summary for this issue to facilitate the next action.");
+            let question = format!("{issue_body}, concentrate on the principal arguments, suggested solutions, and areas of consensus or disagreement among the participants. From these elements, generate a concise summary of the entire issue to inform the next course of action.");
 
             match openai.chat_completion(&chat_id, &question, &co).await {
                 Ok(r) => {
@@ -178,6 +170,6 @@ async fn handler(trigger: &str, owner: &str, repo: &str, payload: EventPayload) 
         }
 
         let text = format!("Issue Summary:\n{}\n{}", _summary, issue_url);
-        // send_message_to_channel(&slack_workspace, &slack_channel, text);
+        send_message_to_channel(&slack_workspace, &slack_channel, text);
     }
 }
